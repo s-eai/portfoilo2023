348c132d96a3098935d18aa4e854b413bff2244b
SE-A <139292851+s-eai@users.noreply.github.com>
2023-11-04 00:29:14 +0900

    Add files via upload

---
+++ js/sea.js
@@ -0,0 +1,315 @@
+$(document).ready(function(){
+    // < 타이틀 하단 버튼 호버 >
+    $("#M > a").hover(
+        function(){
+            $("#M > a span").css({"width" : "100px" , "background" : "none" , "transform" : "translateY(-20px)"}).text(">> Click <<");
+        }, function(){
+            $("#M > a span").css({"width" : "10px" , "background" : "slateblue" , "transform" : "translateY(0px)"}).text("");
+    });// hover END
+
+    // < 팝업 메뉴 버튼 이미지 변환 & mouseenter / mouseleave >
+    const seaBefore = $("#sea-btn img").attr("src");
+    const seaAfter = seaBefore.replace(".png","-click.png");
+    const seaReChange = seaBefore.replace("-click.png" , ".png");
+
+    $("#sea-btn").click(function(){
+        if ( $(this).children().attr("src") == "./img/fix-menu.png" ){
+            $(this).children().attr("src", seaAfter );
+            $("#sea-menu").stop().slideDown();
+        }
+        else {
+            $(this).children().attr("src", seaReChange );
+            $("#sea-menu").stop().slideUp();
+        }
+    });
+
+    $("#sea-btn").on("mouseenter focus" , function(){
+        $(this).children().attr("src", seaAfter );
+        $("#sea-menu").stop().slideDown();
+    })
+    $("#sea").mouseleave(function(){
+        $(this).children().children().attr("src", seaReChange );
+        $("#sea-menu").stop().slideUp();
+    });
+    $("#sea").blur(function(){
+        $(this).children().children().attr("src", seaReChange );
+        $("#sea-menu").stop().slideUp();
+    });
+    $("#sea-menu li a").click(function(){
+        $("#sea-menu").stop().slideUp();
+    });
+
+    // < 스크롤 높이 값에 반응하는 스크립트>
+    const mTop = $("#M").offset().top;
+    const aTop = $("#A").offset().top;
+    const wTop = $("#W1").offset().top;
+    const wwTop = $("#W2").offset().top;
+    const dTop = $("#D").offset().top;
+    const eTop = $("#E").offset().top;
+    let scrollTop = 0;
+
+    $("#layout").scroll(function(){
+        scrollTop = $("#layout").scrollTop();
+
+        if( scrollTop == mTop ){
+            $("#side").hide();
+            $("#sea").hide();
+
+            $("#W1 img").css({ "left" : "-1000px" , "opacity" : "0" });
+            $("#W1 .web-info").css({ "right" : "-500px" , "opacity" : "0" });
+            $("#W2 img").css({ "top" : "-800px" , "opacity" : "0" });
+            $("#W2 .web-info").css({ "bottom" : "-800px" , "opacity" : "0" });
+        }
+
+        if( scrollTop == aTop ){
+            $("#sea").show();
+
+            $("#side").show().css( "display" , "flex" );
+            $("#side li p").hide();
+            $("#side li:nth-child(2) p").show();
+            $("#side li a").removeClass("click");
+            $("#side li:nth-child(2) a").addClass("click");
+
+            $("#photoshop").delay(300).animate({ value : 93 });
+            $("#illustrator").delay(400).animate({ value : 85 });
+            $("#prototype").delay(500).animate({ value : 70 });
+            $("#html").delay(600).animate({ value : 90 });
+            $("#css").delay(700).animate({ value : 88 });
+            $("#jquery").delay(800).animate({ value : 96 });
+
+            $("#W1 img").css({ "left" : "-1000px" , "opacity" : "0" });
+            $("#W1 .web-info").css({ "right" : "-500px" , "opacity" : "0" });
+            $("#W2 img").css({ "top" : "-800px" , "opacity" : "0" });
+            $("#W2 .web-info").css({ "bottom" : "-800px" , "opacity" : "0" });
+        }
+
+        if ( scrollTop == wTop ){
+            $("#sea").show();
+
+            $("#side li p").hide();
+            $("#side li:nth-child(3) p").show();
+            $("#side li a").removeClass("click");
+            $("#side li:nth-child(3) a").addClass("click");
+
+            $("#W1 img").css({ "left" : "0" , "opacity" : "1" });
+            $("#W1 .web-info").css({ "right" : "0" , "opacity" : "1" });
+
+            $("#W2 img").css({ "top" : "-800px" , "opacity" : "0" });
+            $("#W2 .web-info").css({ "bottom" : "-800px" , "opacity" : "0" });
+        }
+
+        if ( scrollTop == wwTop ){
+            $("#sea").show();
+
+            $("#W2 img").css({ "top" : "0" , "opacity" : "1" });
+            $("#W2 .web-info").css({ "bottom" : "0" , "opacity" : "1" });
+
+            $("#W1 img").css({ "left" : "-1000px" , "opacity" : "0" });
+            $("#W1 .web-info").css({ "right" : "-500px" , "opacity" : "0" });
+        }
+
+        if ( scrollTop == dTop ){
+            $("#sea").show();
+
+            $("#side li p").hide();
+            $("#side li:nth-child(4) p").show();
+            $("#side li a").removeClass("click");
+            $("#side li:nth-child(4) a").addClass("click");
+
+            $("#W1 img").css({ "left" : "-1000px" , "opacity" : "0" });
+            $("#W1 .web-info").css({ "right" : "-500px" , "opacity" : "0" });
+            $("#W2 img").css({ "top" : "-800px" , "opacity" : "0" });
+            $("#W2 .web-info").css({ "bottom" : "-800px" , "opacity" : "0" });
+        }
+
+        if ( scrollTop == eTop ){
+            $("#sea").show();
+
+            $("#side li p").hide();
+            $("#side li:nth-child(5) p").show();
+            $("#side li a").removeClass("click");
+            $("#side li:nth-child(5) a").addClass("click");
+
+            $("#W1 img").css({ "left" : "-1000px" , "opacity" : "0" });
+            $("#W1 .web-info").css({ "right" : "-500px" , "opacity" : "0" });
+            $("#W2 img").css({ "top" : "-800px" , "opacity" : "0" });
+            $("#W2 .web-info").css({ "bottom" : "-800px" , "opacity" : "0" });
+        }
+    });
+
+    // < 사이드 버튼을 클릭 할 때 해당 버튼 색이 바뀌고 이름이 보인다 >
+    $("#side a").click(function(){
+        $("#side .side-txt").stop().fadeOut(300);
+        $(this).prev().stop().fadeIn(300);
+        $("#side a").removeClass("click");
+        $(this).addClass("click");
+    });
+    
+    // < About에서 resume a에 hover하면 검은색으로 바뀐다 >
+    const aboutImgFirst = $("#resume a:first-child img").attr("src");
+    const aboutImgFirstChan =aboutImgFirst.replace(".png","-click.png");
+    const aboutImgFirstReChan = aboutImgFirstChan.replace("-click.png" , ".png");
+    
+    const aboutImgLast = $("#resume a:last-child img").attr("src");
+    const aboutImgLastChan = aboutImgLast.replace(".png","-click.png");
+    const aboutImgLastReChan = aboutImgLastChan.replace("-click.png" , ".png");
+    // < 1 >
+    $("#resume a:first-child").on("mouseenter focus" , function(){
+        $(this).children("img").attr("src", aboutImgFirstChan );
+    })
+    $("#resume a:first-child").mouseleave(function(){
+        $(this).children("img").attr("src", aboutImgFirstReChan );
+    });
+    $("#resume a:first-child").mouseleave(function(){
+        $(this).children("img").attr("src", aboutImgFirstReChan );
+    });
+    // < 2 >
+    $("#resume a:last-child").on("mouseenter focus" , function(){
+        $(this).children("img").attr("src", aboutImgLastChan );
+    })
+    $("#resume a:last-child").mouseleave(function(){
+        $(this).children("img").attr("src",aboutImgLastReChan );
+    });
+    $("#resume a:last-child").mouseleave(function(){
+        $(this).children("img").attr("src", aboutImgLastReChan );
+    });
+
+    // < 디테일 디자인에서 작은 이미지에 마우스를 올리면 큰 이미지에 뜬다 >
+    $("#D #small button").on("mouseenter focus", function(){
+        const smallSrc = $(this).children().attr("src");
+        const smallAlt = $(this).children().attr("alt");
+        $("#D #big img").attr({ src : smallSrc , alt : smallAlt });
+        $("#D #small button").css( "filter" , "grayscale(0%)");
+        $(this).css( "filter" , "grayscale(100%)");
+    });
+    $("#D .background").mouseleave(function(){
+        $("#D #small button").css( "filter" , "grayscale(0%)");
+    });
+    $("#D .background").blur(function(){
+        $("#D #small button").css( "filter" , "grayscale(0%)");
+    });
+
+    // < 이벤트 디자인 슬라이드 > 
+    // < 이벤트-웹 / 위에서 아래 >
+    // < 마우스 올리면 슬라이드 멈춤 >
+    let ewSlide = setInterval ( ewSlideDown , 3500 );
+
+    function ewSlideDown(){
+        $("#e-web-slide").stop().animate( { top : "-175px" }, 3400, function(){
+            $("#e-web-slide").prepend( $("#e-web-slide").children().last() );
+            $("#e-web-slide").css( "top" , "-555px" );
+        });
+    }
+    $(".e-web-slide-img img").on("mouseenter focus" , function(){
+        $(".e-web-slide-img .event-info").fadeOut();
+        $(this).next().fadeIn();
+
+        clearInterval( ewSlide );
+    });
+    $("#e-web-slider").mouseleave(function(){
+        $(".e-web-slide-img .event-info").fadeOut();
+        ewSlide = setInterval ( ewSlideDown , 3500 );
+    });
+    $("#e-web-slider").blur(function(){
+        $(".e-web-slide-img .event-info").fadeOut();
+        ewSlide = setInterval ( ewSlideDown , 3500 );
+    });
+
+    // < 이벤트 디자인 슬라이드 > 
+    // < 이벤트-웹 / 위에서 아래 >
+    // < 마우스 올리면 슬라이드 멈춤 >
+    let esSlide = setInterval( esSlideUp , 3500 );
+
+    function esSlideUp(){
+        $("#e-sns-slide").stop().animate( { top : "-585px" }, 3400, function(){
+            $("#e-sns-slide").append( $("#e-sns-slide").children().first() );
+            $("#e-sns-slide").css( "top" , "-205px" );
+        });
+    }
+    $(".e-sns-slide-img img").on("mouseenter focus" , function(){
+        $(".e-sns-slide-img .event-info").fadeOut();
+        $(this).next().fadeIn();
+        clearInterval( esSlide );
+    });
+    $("#e-sns-slider").mouseleave(function(){
+        $(".e-sns-slide-img .event-info").fadeOut();
+        esSlide = setInterval( esSlideUp , 3500 );
+    });
+    $("#e-sns-slider").blur(function(){
+        $(".e-sns-slide-img .event-info").fadeOut();
+        esSlide = setInterval( esSlideUp , 3500 );
+    });
+    // < 디테일 디자인에서 큰 이미지에 마우스를 올리면 자세히 보기가 뜨고 자세히 보기를 누르면 #B가 뜬다 >
+    $("#D #big img").on("mouseenter focus" , function(){
+        $(this).parent().next().fadeIn();
+    });
+    $("#D #big").mouseleave(function(){
+        $("#d-info").fadeOut();
+    });
+    $("#D #big").blur(function(){
+        $("#d-info").fadeOut();
+    });
+
+    $("#d-info").click(function(){
+        const detailBigImgSrc = $(this).prev().children().attr("src");
+        const detailBigImgSrcChan = detailBigImgSrc.replace( ".jpg" , "-big.jpg" );
+        const detailBigImgAlt = $(this).prev().children().attr("alt");
+    
+        $("#side").hide();
+        $("#sea").hide();
+        $("#B").fadeIn();
+        $("#B h4").text( detailBigImgAlt );
+        $("#B img").attr({ "src" : detailBigImgSrcChan , "alt" : detailBigImgAlt });
+        $("#layout").css( "overflow-y" , "hidden" );
+        $("#B").css("overflow-y" , "auto" );
+
+        const seaHeight = $("#sea").position().top;
+        $("#B").css("top" , seaHeight );
+        $("#b-img").css("top" , seaHeight + 100 );
+    });
+
+    // < 이벤트 디자인에서 자세히 보기를 누르면 #B가 뜬다  웹용 > 
+    $("#E .event-info").click(function(){
+        const eventImgSrc = $(this).prev().attr("src");
+        const eventImgSrcChan = eventImgSrc.replace( ".jpg" , "-big.jpg" );
+        const eventImgAlt = $(this).prev().attr("alt");
+
+        clearInterval( ewSlide );
+        clearInterval( esSlide );
+        $("#side").hide();
+        $("#sea").hide();
+        $("#B").fadeIn();
+        $("#B h4").text( eventImgAlt );
+        $("#B img").attr({ "src" : eventImgSrcChan , "alt" : eventImgAlt });
+        $("#b-img").css("width","100vw");
+        $("#layout").css( "overflow-y" , "hidden" );
+        $("#B").css("overflow-y" , "auto" );
+
+        const seaHeight = $("#sea").position().top;
+
+        $("#B").css("top" , seaHeight );
+        $("#b-img").css("top" , seaHeight + 100 );
+    });
+    // < 이벤트 디자인에서 자세히 보기를 누르면 #B가 뜬다  sns용 >
+
+    // < 팝업 창 >
+    $("#b-btn").click(function(){
+        $("#B").fadeOut();
+        $("#B").scrollTop(0);
+        $("#layout").css("overflow-y" , "auto" );
+        $("#side").show().css( "display","flex" );
+        $("#sea").show();
+    });
+
+
+}); //END ALL
+
+$(document).keydown(function(e){
+    if ( e.key == "Escape" ){
+        $("#B").fadeOut();
+        $("#B").scrollTop(0);
+        $("#layout").css("overflow-y" , "auto" );
+        $("#side").show().css( "display","flex" );
+        $("#sea").show();
+    }
+});
